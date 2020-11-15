3d8ebf635c708d026ca261a4f55179570876350f
frankylox <73943575+frankylox@users.noreply.github.com>
2020-11-15 17:02:09 +0300

    Update main_clicker.py
    все еще простоватенько, но хотя бы работает ㅠㅠ

--- main_clicker.py
+++ main_clicker.py
@@ -1,16 +1,17 @@
 import tkinter as tk
+import os
 
 root = tk.Tk()
 root.title('Clicker')
 
 info = tk.Label(text='Нажми <ПРОБЕЛ>, чтобы кликнуть')
 info.pack()
-info = tk.Label(text='Каждый апгрейд стоит 100 кликов и добавляет +1 к очкам за клик')
+info = tk.Label(text='Каждый апгрейд стоит 100 очков и добавляет +1 к очкам за клик')
 btn = tk.Button(text='Click Me for upgrade!', width=15, height=7)
 
 info.pack()
 
-n = 0  # переменная для счетчика
+n = 0
 a = 1
 
 
@@ -31,8 +32,9 @@ def aplus(event):
 
 
 btn.bind('<Button-1>', aplus)
-root.bind("<space>", nplus)
-label = tk.Label(root, text=str(n), font='Helvetica')
+root.bind("<KeyRelease-space>", nplus)
+
+label = tk.Label(root, text=str(n))
 label.pack()
 
 btn.pack()
